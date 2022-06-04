30753650c0701cc8e1976667359f826ec3494782
jinjianan <jinjiananchn@gmail.com>
2022-06-04 12:45:37 +0800

    二分搜索

---
+++ src/main/java/serach/BinarySearch.java
@@ -0,0 +1,35 @@
+package serach;
+
+public class BinarySearch {
+
+    private BinarySearch() {
+    }
+
+    public static <T extends Comparable<T>> int search(T[] arr, T t) {
+        return searchRecursion(arr, 0, arr.length - 1, t);
+    }
+
+    public static <T extends Comparable<T>> int searchRecursion(T[] arr, int l, int r, T t) {
+        if (r < l) return -1;
+        int mid = (r - l) / 2 + l;
+        if (arr[mid].compareTo(t) == 0) return mid;
+        else if (arr[mid].compareTo(t) > 0) return searchRecursion(arr, l, mid - 1, t);
+        else return searchRecursion(arr, mid + 1, r, t);
+    }
+
+    public static <T extends Comparable<T>> int search2(T[] arr, T t) {
+
+        int l = 0;
+        int r = arr.length - 1;
+        while (r >= l) {
+            int mid = (r - l) / 2;
+            if (arr[mid].compareTo(t) == 0) return mid;
+            if (arr[mid].compareTo(t) > 0)
+                r = mid - 1;
+            else
+                l = mid + 1;
+        }
+        return -1;
+    }
+
+}
